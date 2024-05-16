b40d9baa8ee19c9f22eff379dc94762b8c08e6e7
“Brenno <dev.brennoeudes@gmail.com>
2024-05-16 16:14:19 -0300

    tag component

--- MyMovies/src/pages/Details/index.jsx
+++ MyMovies/src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -10,10 +11,10 @@ export function Details() {
         <Header />
         <h1>Hello, world!</h1>
         <Section>
-          
+          <Tag title="SciFi"/>
+          <Tag title="Drama"/>
+          <Tag title="Family"/>
         </Section>
-  
-
       </Container>
     </>
   );
