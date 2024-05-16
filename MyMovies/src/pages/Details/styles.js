8276be784bb242a69e52a068fe4f966fe94f02f9
“Brenno <dev.brennoeudes@gmail.com>
2024-05-16 19:40:47 -0300

    scroll bar stylization

--- MyMovies/src/pages/Details/styles.js
+++ MyMovies/src/pages/Details/styles.js
@@ -15,6 +15,21 @@ export const Container = styled.div`
     grid-area: content; /* Usando o main p/ aplicar região do grid. */
     overflow-y: scroll; /* Barra de rolagem vertical */
     padding: 6.4rem 0;
+    max-height: 80vh; /* Adjust this value to control the visible height of the content */
+
+     /* Custom scrollbar styles */
+     &::-webkit-scrollbar {
+      width: 1.2rem; /* Width of the scrollbar */
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: ${({ theme }) => theme.COLORS.PINK}; /* Color of the scrollbar thumb */
+      border-radius: .6rem;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+      background-color: #555; /* Color of the scrollbar thumb on hover */
+    }
   }
 `;
 
