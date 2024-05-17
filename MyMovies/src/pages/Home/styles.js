5d53e911c8c3ad581f03f202c829279904ba1d03
“Brenno <dev.brennoeudes@gmail.com>
2024-05-17 16:50:38 -0300

    text -webkit-line-clamp adjust

--- MyMovies/src/pages/Home/styles.js
+++ MyMovies/src/pages/Home/styles.js
@@ -61,5 +61,17 @@ export const Content = styled.div`
 
   > p {
     text-align: justify;
+
+    display: -webkit-box; // A display type that is necessary for the line-clamp feature to work.
+    -webkit-line-clamp: 2; // The number of lines to display before truncating.
+    -webkit-box-orient: vertical; // Sets the orientation of the box to be vertical.
+    overflow: hidden; // Hides the overflowing text.
+    text-overflow: ellipsis; // Adds an ellipsis (...) to indicate the text has been truncated.
+    
+    height: 4rem; /* Adjust this based on your line-height */
+    line-height: 2rem; /* Adjust this based on your line-height */
+
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+
   }
 `;
