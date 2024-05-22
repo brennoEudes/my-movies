17d778fe98d27c3863bd5d043e08866bb2053d8a
“Brenno <dev.brennoeudes@gmail.com>
2024-05-22 12:41:50 -0300

    section component adjust

--- MyMovies/src/components/Section/styles.js
+++ MyMovies/src/components/Section/styles.js
@@ -4,4 +4,9 @@ export const Container = styled.section`
   color: ${({ theme }) => theme.COLORS.GRAY_100};
   font-size: 2rem;
   font-weight: 400;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
 `;
