193de7338df054f8a43c9d742d8f5b09eed34492
“Brenno <dev.brennoeudes@gmail.com>
2024-07-09 12:13:00 -0300

    refactorizatoin, node_modules, packages jason deleted, reinstall dependences

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,5 +1,5 @@
 import stars from "../../assets/stars.svg";
-import { Tag } from "../../components/Tag";
+import { Tag } from "../Tag";
 import { Container } from "./styles";
 
 export function MovieCard({ data, ...rest }) {
